01c088ac88fbdc1c49dd3ac32b461ac5430f4a63
dative001 <dative.mukangina01@gmail.com>
2023-09-06 16:34:59 -0700

    sep06 commit

--- ForJavaScript/TrialFor.js
+++ ForJavaScript/TrialFor.js
@@ -1,3 +1,6 @@
+<html>
+    <body>
+        <script>
 // Task 1: Build a function-based console log message generator
 function consoleStyler(color, background, fontSize, txt) {
     var message = "%c" + txt;
@@ -31,3 +34,53 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
 }
 // Call styleAndCelebrate
 styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it', 'champions');
+// Task 1: Code a Person class
+class Person{
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+        console.log(this.energy);
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+        console.log(this.energy);
+ }   
+}
+// Task 2: Code a Worker class
+class Worker extends Person{
+    constructor( name, age, energy, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage
+            
+    }
+    goToWork() {
+        this.xp += 10;
+        console.log(this.xp);
+   } 
+}
+
+// Task 3: Code an intern object, run methods
+function intern() {
+    var intern = new Worker("Bob", 21, 110, 0, 10);
+    intern.goToWork();
+    return intern;
+}
+ console.log(intern());
+// Task 4: Code a manager object, methods
+function manager() {
+    var manager = new Worker("Alice", 30, 120, 100, 30);
+    manager.doSomethingFun();
+    return manager;
+}
+    console.log(manager());
+        </script>
+    </body>
+</html>
+
+
+
